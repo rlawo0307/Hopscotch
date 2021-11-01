3769d2fc4f0cae74f1b0609949651de2d937c385
rlawo0307 <rlawo0307@naver.com>
2021-11-01 10:53:34 +0900

    초기 커밋.

---
+++ Main.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace PacMan
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Play
+            // 
+            this.Btn_Play.Location = new System.Drawing.Point(327, 242);
+            this.Btn_Play.Name = "Btn_Play";
+            this.Btn_Play.Size = new System.Drawing.Size(75, 49);
+            this.Btn_Play.TabIndex = 0;
+            this.Btn_Play.Text = "PLAY";
+            this.Btn_Play.UseVisualStyleBackColor = true;
+            this.Btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_Play);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Play;
+    }
+}
+
