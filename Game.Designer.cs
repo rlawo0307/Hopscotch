72ec2f7f5b36a8e8b7199f7cb52839a48b1e645c
rlawo0307 <rlawo0307@naver.com>
2021-11-02 14:09:43 +0900

    .

--- Game.Designer.cs
+++ Game.Designer.cs
@@ -30,6 +30,7 @@ namespace Hopscotch
         private void InitializeComponent()
         {
             this.ImageBoard = new System.Windows.Forms.PictureBox();
+            this.Btn_Player = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ImageBoard)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,11 +42,24 @@ namespace Hopscotch
             this.ImageBoard.TabIndex = 0;
             this.ImageBoard.TabStop = false;
             // 
+            // Btn_Player
+            // 
+            this.Btn_Player.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Btn_Player.FlatAppearance.BorderSize = 0;
+            this.Btn_Player.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Player.Location = new System.Drawing.Point(27, 39);
+            this.Btn_Player.Name = "Btn_Player";
+            this.Btn_Player.Size = new System.Drawing.Size(75, 44);
+            this.Btn_Player.TabIndex = 1;
+            this.Btn_Player.Text = "player";
+            this.Btn_Player.UseVisualStyleBackColor = true;
+            // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_Player);
             this.Controls.Add(this.ImageBoard);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -59,5 +73,6 @@ namespace Hopscotch
         #endregion
 
         private System.Windows.Forms.PictureBox ImageBoard;
+        private System.Windows.Forms.Button Btn_Player;
     }
 }
--- Game.cs
+++ Game.cs
@@ -10,24 +10,40 @@ using System.Windows.Forms;
 
 namespace Hopscotch
 {
+    static class Constants
+    {
+        public const int Form_Width = 600, Form_Height = 500;
+        public const int Board_Width = 570, Board_Height = 450;
+        public const int Player_Width = 10, Player_Height = 10;
+        public const string ImagePath = "./res/Image/Image1.jpg";
+    }
+
     public partial class Game : Form
     {
+        private int player_x;
+        private int player_y;
+
         public Game()
         {
             InitializeComponent();
-            this.Size = new Size(600, 500);
-            ImageBoard.Size = new Size(570, 450);
+
+            this.Size = new Size(Constants.Form_Width, Constants.Form_Height);
+            ImageBoard.Size = new Size(Constants.Board_Width, Constants.Board_Height);
             ImageBoard.SizeMode = PictureBoxSizeMode.StretchImage;
-            ImageBoard.Image = Image.FromFile("./res/Image/Image1.jpg");
+            ImageBoard.Image = Image.FromFile(Constants.ImagePath);
 
             Graphics g = Graphics.FromImage((System.Drawing.Image)ImageBoard.Image);
-            DrawRec(g, 0,0, ImageBoard.Image.Width, ImageBoard.Height);
+            DrawRec(g, 0, 0, ImageBoard.Image.Width, ImageBoard.Height);
 
-            //SolidBrush sb = new SolidBrush(Color.Red);
-            //g.DrawEllipse(Pens.Red, -1, -1, 10, 10);
+            Btn_Player.Size = new Size(Constants.Player_Width, Constants.Player_Height);
+            player_y = ImageBoard.Location.Y - Constants.Player_Height / 2;
+            PlayerMove(player_x, player_y);
+            Btn_Player.BackColor = Btn_Player.ForeColor = Btn_Player.FlatAppearance.BorderColor = Color.Red;
 
-            Play();
-            }
+            KeyPreview = true;
+            this.KeyDown += Key_Down;
+
+        }
 
         private void Play()
         {
@@ -41,6 +57,22 @@ namespace Hopscotch
 
             g.FillRectangle(sb, rec);
             sb.Dispose();
+        }
+
+        private void PlayerMove(int x, int y)
+        {
+            Btn_Player.Location = new Point(x, y);
+        }
+
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A: PlayerMove(--player_x, player_y); break;
+                case Keys.D: PlayerMove(++player_x, player_y); break;
+                case Keys.S: PlayerMove(player_x, ++player_y); break;
+                case Keys.W: PlayerMove(player_x, --player_y); break;
+            }
         }
     }
 }
