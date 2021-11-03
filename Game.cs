113e860ae3caeb9d4cdb14f52f26479882664142
rlawo0307 <rlawo0307@naver.com>
2021-11-03 14:17:45 +0900

    .

--- Game.cs
+++ Game.cs
@@ -12,9 +12,22 @@ namespace Hopscotch
 {
     public partial class Game : Form
     {
-        private int player_x;
-        private int player_y;
-        Button player;
+        static class Constants
+        {
+            public const int Form_Width = 600, Form_Height = 500;
+            public const int Board_Width = 570, Board_Height = 450;
+            public const int Player_Width = 10, Player_Height = 10;
+            //public const string ImagePath = "./res/Image/Image1.jpg";
+        }
+
+        class Player
+        {
+            public int x;
+            public int y;
+        }
+
+        Player player;
+        Button btn_player;
         Graphics g;
         SolidBrush sb;
         Panel board;
@@ -31,17 +44,18 @@ namespace Hopscotch
             //g = Graphics.FromImage((System.Drawing.Image)ImageBoard.Image);
             //g.FillRectangle(new SolidBrush(Color.Gray), new Rectangle(0, 0, ImageBoard.Image.Width, ImageBoard.Image.Height));
 
-            player_x = 50;
-            player_y = 50;
+            player = new Player();
+            player.x = 50;
+            player.y = 50;
 
-            player = new Button();
-            player.Size = new Size(Constants.Player_Width, Constants.Player_Height);
-            player.Location = new Point(player_x, player_y);
-            player.BackColor = Color.White;
-            player.FlatAppearance.BorderSize = 0;
-            player.FlatStyle = FlatStyle.Flat;
-            player.Enabled = false;
-            this.Controls.Add(player);
+            btn_player = new Button();
+            btn_player.Size = new Size(Constants.Player_Width, Constants.Player_Height);
+            btn_player.Location = new Point(player.x, player.y);
+            btn_player.BackColor = Color.White;
+            btn_player.FlatAppearance.BorderSize = 0;
+            btn_player.FlatStyle = FlatStyle.Flat;
+            btn_player.Enabled = false;
+            this.Controls.Add(btn_player);
 
             board = new Panel();
             board.Size = new Size(Constants.Board_Width, Constants.Board_Height);
@@ -61,29 +75,23 @@ namespace Hopscotch
         private void Key_Down(object sender, KeyEventArgs e)
         {
             
-            int prev_x = player_x, prev_y = player_y;
+            int prev_x = player.x, prev_y = player.y;
 
             if (e.KeyCode == Keys.A || e.KeyCode == Keys.D || e.KeyCode == Keys.S || e.KeyCode == Keys.W)
             {
                 switch (e.KeyCode)
                 {
-                    case Keys.A: player_x-=5; break;
-                    case Keys.D: player_x+=5; break;
-                    case Keys.S: player_y+=5; break;
-                    case Keys.W: player_y-=5; break;
+                    case Keys.A: player.x -= 5; break;
+                    case Keys.D: player.x += 5; break;
+                    case Keys.S: player.y += 5; break;
+                    case Keys.W: player.y -= 5; break;
                 }
-                player.Location = new Point(player_x, player_y);
+                btn_player.Location = new Point(player.x, player.y);
                 g.FillRectangle(sb, new Rectangle(prev_x, prev_y, Constants.Player_Width, Constants.Player_Height));
                 //sb.Dispose();
             }
         }
     }
 
-    static class Constants
-    {
-        public const int Form_Width = 600, Form_Height = 500;
-        public const int Board_Width = 570, Board_Height = 450;
-        public const int Player_Width = 10, Player_Height = 10;
-        //public const string ImagePath = "./res/Image/Image1.jpg";
-    }
+    
 }
