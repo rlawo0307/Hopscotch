5e8ffb32b89daeb5ef1ff6117be35433dfe20607
rlawo0307 <rlawo0307@naver.com>
2021-11-03 15:16:13 +0900

    .

--- Game.cs
+++ Game.cs
@@ -15,15 +15,21 @@ namespace Hopscotch
         static class Constants
         {
             public const int Form_Width = 600, Form_Height = 500;
-            public const int Board_Width = 570, Board_Height = 450;
+            public const int Board_Width = 500, Board_Height = 400;
             public const int Player_Width = 10, Player_Height = 10;
             //public const string ImagePath = "./res/Image/Image1.jpg";
         }
 
         class Player
         {
-            public int x;
-            public int y;
+            public Point start_point;
+            public Point cur_point;
+
+            public Player()
+            {
+                start_point = new Point(0, 0);
+                cur_point = new Point(0,0);
+            }  
         }
 
         Player player;
@@ -40,17 +46,14 @@ namespace Hopscotch
             //ImageBoard.Size = new Size(Constants.Board_Width, Constants.Board_Height);
             //ImageBoard.SizeMode = PictureBoxSizeMode.StretchImage;
             //ImageBoard.Image = Image.FromFile(Constants.ImagePath);
-
             //g = Graphics.FromImage((System.Drawing.Image)ImageBoard.Image);
             //g.FillRectangle(new SolidBrush(Color.Gray), new Rectangle(0, 0, ImageBoard.Image.Width, ImageBoard.Image.Height));
 
             player = new Player();
-            player.x = 50;
-            player.y = 50;
 
             btn_player = new Button();
             btn_player.Size = new Size(Constants.Player_Width, Constants.Player_Height);
-            btn_player.Location = new Point(player.x, player.y);
+            btn_player.Location = player.cur_point;
             btn_player.BackColor = Color.White;
             btn_player.FlatAppearance.BorderSize = 0;
             btn_player.FlatStyle = FlatStyle.Flat;
@@ -74,22 +77,31 @@ namespace Hopscotch
 
         private void Key_Down(object sender, KeyEventArgs e)
         {
-            
-            int prev_x = player.x, prev_y = player.y;
+            Point prev_point = player.cur_point;
 
             if (e.KeyCode == Keys.A || e.KeyCode == Keys.D || e.KeyCode == Keys.S || e.KeyCode == Keys.W)
             {
                 switch (e.KeyCode)
                 {
-                    case Keys.A: player.x -= 5; break;
-                    case Keys.D: player.x += 5; break;
-                    case Keys.S: player.y += 5; break;
-                    case Keys.W: player.y -= 5; break;
+                    case Keys.A: if(player.cur_point.X > 0) player.cur_point.X -= 5; break;
+                    case Keys.D: if (player.cur_point.X + Constants.Player_Width < Constants.Board_Width) player.cur_point.X += 5; break;
+                    case Keys.S: if (player.cur_point.Y + Constants.Player_Height < Constants.Board_Height) player.cur_point.Y += 5; break;
+                    case Keys.W: if(player.cur_point.Y > 0) player.cur_point.Y -= 5; break;
                 }
-                btn_player.Location = new Point(player.x, player.y);
-                g.FillRectangle(sb, new Rectangle(prev_x, prev_y, Constants.Player_Width, Constants.Player_Height));
+                btn_player.Location = player.cur_point;
+                g.FillRectangle(sb, new Rectangle(prev_point.X, prev_point.Y, Constants.Player_Width, Constants.Player_Height));
                 //sb.Dispose();
+
+                MessageBox.Show("" + g.);
+
+                CheckBound();
             }
+        }
+
+        private void CheckBound()
+        {
+            ;
+            
         }
     }
 
